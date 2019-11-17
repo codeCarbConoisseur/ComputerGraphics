6a721d4e1cdd83d49cd8c996faa85ea75c3ec318
sofvckinmadguy <segalzakhar1999@mail.ru>
2019-11-17 17:56:17 +0300

    Initial Commit

---
+++ MatrixKG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatrixKG
+//
+//  Created by Захар  Сегал on 17.11.2019.
+//  Copyright © 2019 Захар  Сегал. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
